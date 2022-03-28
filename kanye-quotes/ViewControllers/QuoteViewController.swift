ec65cc0ebb9e7a9a0a1e6c982e2acbe9d7494cc2
npappas27 <nickpappas@austins-mbp.lan>
2022-03-28 00:32:02 -0400

    inital project setup, init quote

--- kanye-quotes/ViewControllers/QuoteViewController.swift
+++ kanye-quotes/ViewControllers/QuoteViewController.swift
@@ -10,31 +10,46 @@ import UIKit
 class QuoteViewController: UIViewController {
     
     var quoteLabel = UILabel(frame: .zero)
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        configureQuoteLabel()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getInitialQuote()
         
+    }
+    
+    private func getInitialQuote() {
         NetworkManager.shared.getQuote { quote in
-            print(quote)
+            if let quote = quote {
+                DispatchQueue.main.async { [weak self] in
+                    guard let self = self else { return }
+                    self.quoteLabel.text = quote
+                }
+            }
         }
     }
     
     
-    func configureQuoteLabel() {
+    private func configureQuoteLabel() {
         let padding: CGFloat = 12
         view.addSubview(quoteLabel)
         quoteLabel.text = "I'm giving all Good music artists back the 50% share I have of their masters" // default
         quoteLabel.translatesAutoresizingMaskIntoConstraints = false
         quoteLabel.textColor = .label
         quoteLabel.font = UIFont.systemFont(ofSize: 18)
-        quoteLabel.numberOfLines = 0
+        quoteLabel.textAlignment = .center
+        quoteLabel.numberOfLines = 4
         NSLayoutConstraint.activate([
             quoteLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             quoteLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
-            quoteLabel.leadingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
-            quoteLabel.heightAnchor.constraint(equalToConstant: 200)
+            quoteLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            quoteLabel.heightAnchor.constraint(equalToConstant: 400)
         ])
     }
-
+    
 }
